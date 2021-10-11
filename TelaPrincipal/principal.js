16423122fede30a163e6ce3bda32b894dd342921
Marcos <marcosvhd1@gmail.com>
2021-10-11 15:32:45 -0300

    Tela de Login

--- TelaPrincipal/principal.js
+++ TelaPrincipal/principal.js
@@ -1,15 +1,25 @@
 let BtnInicio = document.querySelector(".BtnInicio");
 let BtnCliente = document.querySelector(".BtnClientes");
+let BtnProdutos = document.querySelector(".BtnProdutos");
 
 const TelaClientes = document.getElementById("Clientes");
 const TelaInicio = document.getElementById("Inicio");
+const TelaProdutos = document.getElementById("Produtos");
 
 BtnCliente.onclick = function(){
     TelaClientes.style.display = "block";
     TelaInicio.style.display = "none";
+    TelaProdutos.style.display = "none";
 }
 
 BtnInicio.onclick = function(){
   TelaClientes.style.display = "none";
   TelaInicio.style.display = "flex";
+  TelaProdutos.style.display = "none";
+}
+
+BtnProdutos.onclick = function(){
+  TelaClientes.style.display = "none";
+  TelaInicio.style.display = "none";
+  TelaProdutos.style.display = "block";
 }
